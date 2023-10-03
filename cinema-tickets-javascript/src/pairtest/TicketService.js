0b407c204c80cc7981ab9faef261cf8014945fcc
Moosa Shah <hello.moosashah@gmail.com>
2023-10-03 16:07:33 +0100

    implement TicketService and tests

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,12 +1,67 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketTypeRequest from './lib/TicketTypeRequest.js'
+import InvalidPurchaseException from './lib/InvalidPurchaseException.js'
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
+  #ticketPaymentService
+  #seatReservationService
+
+  constructor(ticketPamentService, seatReservationService) {
+    this.#ticketPaymentService = ticketPamentService
+    this.#seatReservationService = seatReservationService
+  }
+
+  #validateRequests(requests) {
+    const totalTickets = requests.reduce(
+      (sum, req) => sum + req.getNoOfTickets(),
+      0
+    )
+
+    if (totalTickets > 20) {
+      throw new InvalidPurchaseException(
+        'Cannot purchase more than 20 tickets at once.'
+      )
+    }
+
+    const adultTickets =
+      requests
+        .find((req) => req.getTicketType() === 'ADULT')
+        ?.getNoOfTickets() || 0
+    if (adultTickets === 0 && totalTickets > 0) {
+      throw new InvalidPurchaseException(
+        'Child or Infant tickets cannot be purchased without an Adult ticket.'
+      )
+    }
+  }
+  #calculateTotalCost(req) {
+    return req.reduce((sum, req) => {
+      if (req.getTicketType() === 'ADULT')
+        return sum + 20 * req.getNoOfTickets()
+      if (req.getTicketType() === 'CHILD')
+        return sum + 10 * req.getNoOfTickets()
+      return sum
+    }, 0)
+  }
+
+  #calculateTotalSeatsToReserve(req) {
+    return req.reduce((sum, req) => {
+      if (req.getTicketType() !== 'INFANT') return sum + req.getNoOfTickets()
+      return sum
+    }, 0)
+  }
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
-    // throws InvalidPurchaseException
+    if (accountId <= 0) {
+      throw new InvalidPurchaseException('Invalid accountId')
+    }
+
+    this.#validateRequests(ticketTypeRequests)
+    const totalCost = this.#calculateTotalCost(ticketTypeRequests)
+    this.#ticketPaymentService.makePayment(accountId, totalCost)
+
+    const totalSeats = this.#calculateTotalSeatsToReserve(ticketTypeRequests)
+    this.#seatReservationService.reserveSeats(accountId, totalSeats)
   }
 }
---
+++ cinema-tickets-javascript/test/TicketService.test.js
@@ -0,0 +1,68 @@
+import TicketService from '../src/pairtest/TicketService'
+import TicketTypeRequest from '../src/pairtest/lib/TicketTypeRequest'
+import InvalidPurchaseException from '../src/pairtest/lib/InvalidPurchaseException'
+
+const mockPaymentService = {
+  makePayment: jest.fn(),
+}
+
+const mockReservationService = {
+  reserveSeats: jest.fn(),
+}
+
+describe('TicketService', () => {
+  let ticketService
+
+  beforeEach(() => {
+    ticketService = new TicketService(
+      mockPaymentService,
+      mockReservationService
+    )
+    mockPaymentService.makePayment.mockClear()
+    mockReservationService.reserveSeats.mockClear()
+  })
+
+  it('should throw when total tickets exceed 20', () => {
+    const ticketReq = new TicketTypeRequest('ADULT', 21)
+    expect(() => ticketService.purchaseTickets(1, ticketReq)).toThrow(
+      new InvalidPurchaseException(
+        'Cannot purchase more than 20 tickets at once.'
+      )
+    )
+  })
+
+  it('should throw when purchsing child or infant tickets without an adult', () => {
+    const childTicketReq = new TicketTypeRequest('CHILD', 5)
+    const infantTicketReq = new TicketTypeRequest('INFANT', 5)
+    expect(() => ticketService.purchaseTickets(1, childTicketReq)).toThrow(
+      new InvalidPurchaseException(
+        'Child or Infant tickets cannot be purchased without an Adult ticket.'
+      )
+    )
+    expect(() => ticketService.purchaseTickets(1, infantTicketReq)).toThrow(
+      new InvalidPurchaseException(
+        'Child or Infant tickets cannot be purchased without an Adult ticket.'
+      )
+    )
+  })
+
+  it('should make correct payment for adult tickets', () => {
+    const ticketReq = new TicketTypeRequest('ADULT', 5)
+    ticketService.purchaseTickets(1, ticketReq)
+    expect(mockPaymentService.makePayment).toHaveBeenCalledWith(1, 100)
+  })
+
+  it('should make correct payment for child tickets', () => {
+    const adultTicketReq = new TicketTypeRequest('ADULT', 1)
+    const childTicketReq = new TicketTypeRequest('CHILD', 2)
+    ticketService.purchaseTickets(1, adultTicketReq, childTicketReq)
+    expect(mockPaymentService.makePayment).toHaveBeenCalledWith(1, 40)
+  })
+
+  it('should not reserve seats for infants', () => {
+    const adultTicketReq = new TicketTypeRequest('ADULT', 1)
+    const infantTicketReq = new TicketTypeRequest('INFANT', 2)
+    ticketService.purchaseTickets(1, adultTicketReq, infantTicketReq)
+    expect(mockReservationService.reserveSeats).toHaveBeenCalledWith(1, 1)
+  })
+})
